72fdc13ced4e241b112141ff3b52f0161dc3f589
ntdatt812 <ntdat812@gmail.com>
2025-05-13 23:03:31 +0700

    main container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import MainSlider from "@/components/main/main.slider";
+import { Container } from "@mui/material";
 
 export default function HomePage() {
   return (
-    <div>
-      Hello world
-    </div>
+    <Container>
+      <MainSlider />
+    </Container>
   );
 }
