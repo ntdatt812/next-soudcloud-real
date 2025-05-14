df0947ad18c638fc5747a7a10f5cb6c9d99981df
ntdatt812 <ntdat812@gmail.com>
2025-05-14 21:01:11 +0700

    dynamic router

---
+++ src/app/track/[slug]/page.tsx
@@ -0,0 +1,9 @@
+
+const DetailTrackPage = ({ params }: { params: { slug: string } }) => {
+    console.log(">> check id: ", params)
+    return (
+        <div>DetailTrackPage</div>
+    )
+}
+
+export default DetailTrackPage
