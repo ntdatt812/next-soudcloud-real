51d81b6f9f1579e7d6671bd56706f3447fd88ff9
ntdatt812 <ntdat812@gmail.com>
2025-05-14 21:21:06 +0700

    search params

--- src/app/track/[slug]/page.tsx
+++ src/app/track/[slug]/page.tsx
@@ -1,6 +1,10 @@
-
+'use client'
+import { useSearchParams } from 'next/navigation'
 const DetailTrackPage = ({ params }: { params: { slug: string } }) => {
-    console.log(">> check id: ", params)
+    console.log(">> check id: ", params);
+    const searchParams = useSearchParams();
+    const search = searchParams.get('audio');
+    console.log(">>> check audio: ", search)
     return (
         <div>DetailTrackPage</div>
     )
