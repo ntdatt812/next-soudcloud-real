cae6b0ddc20730852ab581ae3a0f24fa6bb45df1
ntdatt812 <ntdat812@gmail.com>
2025-05-14 22:19:14 +0700

    Ref

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,18 +1,22 @@
 'use client'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import WaveSurfer from 'wavesurfer.js'
 
 const WaveTrack = () => {
+
+    const containerRef = useRef<HTMLDivElement>(null);
+
     useEffect(() => {
+        console.log("> check containerRef: ", containerRef.current);
         const wavesurfer = WaveSurfer.create({
-            container: document.getElementById("ntdat")!,
+            container: containerRef.current!,
             waveColor: 'rgb(200, 0, 200)',
             progressColor: 'rgb(100, 0, 100)',
             url: '/tracks/hoidanit.mp3',
         })
     }, [])
     return (
-        <div id="ntdat" className=''>
+        <div ref={containerRef}>
             WaveTrack
         </div>
     )
