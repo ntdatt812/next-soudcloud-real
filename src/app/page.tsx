87028d3f9572b7d26f8b22144ec8c757cf55cbac
ntdatt812 <ntdat812@gmail.com>
2025-05-16 23:04:09 +0700

    config session for server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import MainSlider from "@/components/main/main.slider";
 import { Container } from "@mui/material";
 import { sendRequest } from "@/utils/api";
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function HomePage() {
-
+  const session = await getServerSession(authOptions)
+  console.log(">>> check session:", session)
   const chills = await sendRequest<IBackendRes<ITrackTop[]>>({
     url: "http://localhost:8000/api/v1/tracks/top",
     method: "POST",
